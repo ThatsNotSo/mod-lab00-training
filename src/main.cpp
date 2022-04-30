ee58378862c8de4a0e018797cf8dd5b6389a5958
ThatsNotSo <104660360+ThatsNotSo@users.noreply.github.com>
2022-04-30 09:37:09 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+
 int main() {
   int64_t a, c; uint16_t b;
-   a = 2;
-   b = 10;
-   c = power(a, b);
+    a = 2;
+    b = 10;
+    c = power(a, b);
 }
