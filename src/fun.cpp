d26f123ae92307fbfd632244d809bcb952ce4fb9
ThatsNotSo <104660360+ThatsNotSo@users.noreply.github.com>
2022-05-01 00:14:11 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,6 +1,11 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <math.h>
+#include <iostream>
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+  int y = 1;
+    for (int i = 1; i <= n; i++)
+        y *= x;
+    return y;
 }
